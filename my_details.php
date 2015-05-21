8672d480a1c0c3307d85a8424ea5f9afcecb869e
HowlMarauder <vigneshk618@gmail.com>
2015-05-21 21:27:35 +0530

    Create my_details.php

---
+++ my_details.php
@@ -0,0 +1,11 @@
+<?php
+  echo "<table>"
+  echo "<tr><td>Name</td>"
+  echo "<td>$_GET["name"]</td></tr>"
+  echo "<tr><td>DOB</td>"
+  echo "<td>$_GET["dob"]</td></tr>"
+  echo "<tr><td>Degree</td>"
+  echo "<td>$_GET["degree"]</td></tr>"
+  echo "<tr><td>Department</td>"
+  echo "<td>$_GET["dept"]</td></tr>"
+?>
