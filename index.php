c9cf031e40ba4e1bf2ef993c2979b76e7782d113
HowlMarauder <vigneshk618@gmail.com>
2015-05-21 21:19:32 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<form action="my_details.php" method="GET">
+Name <input type="text" name="name">
+Date Of Birth <input type="text" name="dob">
+Degree <input type="text" name="degree">
+Department <input type="text" name="dept">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
+
+
+
